4aa355cb697f52403641fab173ca0cc5a3175c80
allen <allenw@mit.edu>
2020-05-17 23:06:16 -0500

    upd readme

--- algebraic_moments/test/test_moment_expressions.py
+++ algebraic_moments/test/test_moment_expressions.py
@@ -1,14 +1,14 @@
 from algebraic_moments.objects import RandomVariable, RandomVector, DeterministicVariable
 from algebraic_moments.moment_expressions import generate_moment_expressions
 
-def test_foo():
+def test_moment_expressions():
     x = RandomVariable("x")
     y = RandomVariable("y")
     c = DeterministicVariable("c")
     vector = RandomVector([x, y], [])
-    expressions = [(c * x*y**2 + y)**2, (y*x**2 + c*y**2)**3]
+    expressions = {"g1" : (c * x*y**2 + y)**2, "g2" : (y*x**2 + c*y**2)**3}
     deterministic_variables = [c]
     moment_expressions = generate_moment_expressions(expressions, vector, deterministic_variables)
     moment_expressions.print("matlab")
 
-test_foo()
+test_moment_expressions()
